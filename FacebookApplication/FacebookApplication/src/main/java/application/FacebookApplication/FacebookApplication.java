6313fc63ac79d8e4cbe8f553ab2abff70ea13c44
Alessia Castagnolo <a.castagnolo58@gmail.com>
2020-06-14 17:28:20 +0200

    modifiche al controller e FacebookApplication

--- FacebookApplication/FacebookApplication/src/main/java/application/FacebookApplication/FacebookApplication.java
+++ FacebookApplication/FacebookApplication/src/main/java/application/FacebookApplication/FacebookApplication.java
@@ -1,12 +1,36 @@
 package application.FacebookApplication;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import application.util.Storage;
+
+/**
+ * Spring Boot Application
+ * @author Flavia Gioiello
+ * @author Alessia Castagnolo
+ *
+ */
 @SpringBootApplication
 public class FacebookApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException,IOException,JSONException  {
+		try {
+			Storage.fill_post("https://graph.facebook.com/v7.0/106676254404182/feed?access_token=EAAD21gdlzPcBAA8rIqD9K7VHPpOOyBboGGPQRG1h1lkYd0HKezdVNwTugU1K6xE7HGZANncLoN8D7x6ggXZBjqCzRFu70oxDO1IYQel1lE1hkMvUhkr64TjBzov1dB9CHxnJMIQ5s5nZCDKKlfP09ibU7Ewmhq8A4SW4gZB7CdUv64cT2dGDciMYaZCVaQdwZD");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		SpringApplication.run(FacebookApplication.class, args);
 	}
 
--- FacebookApplication/FacebookApplication/src/main/java/application/FacebookApplication/controller/Controller.java
+++ FacebookApplication/FacebookApplication/src/main/java/application/FacebookApplication/controller/Controller.java
@@ -1,16 +1,40 @@
 package application.FacebookApplication.controller;
 
+
+
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import application.model.Metadata;
 import application.model.Post;
+import application.util.Storage;
 
+/**
+ * Controller
+ * @author Flavia Gioiello
+ * @author Alessia Castagnolo
+ *
+ */
 @RestController
 public class Controller {
+	/**
+	 * Post pubblicati con id e message
+	 * @return elenco dei post
+	 */
 	@GetMapping("/post")
-	public Post getPost(@RequestBody Post body) {
-		return body;
+	public ArrayList<Post> getArrayPost(){
+		return Storage.get_post();
+	}
+	
+	/**
+	 * elenco metadata
+	 * @return elenco dei metadata
+	 */
+	@GetMapping("/metadata")
+	public ArrayList<Metadata> getArrayMetadata(){
+		return Storage.fill_metadata();
 	}
 
 }
