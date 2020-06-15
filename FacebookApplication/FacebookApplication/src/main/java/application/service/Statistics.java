aafccf99e3464933cfc099f338f1be26183f77a4
flaviagioiello <s1087154@studenti.univpm.it>
2020-06-15 22:17:22 +0200

    max metodo

--- FacebookApplication/FacebookApplication/src/main/java/application/service/Statistics.java
+++ FacebookApplication/FacebookApplication/src/main/java/application/service/Statistics.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Set;
 
 import application.model.Emoticon;
@@ -21,18 +22,23 @@ public class Statistics  {
 		E.count(array);
 		H.count(array);
 	}
-	public int max_hashtag(HashMap<String, Integer> h) {
+	public String max_hashtag(HashMap<String, Integer> h) {
 		h=H.getH();
 		int max=0;
+		String key=null;
 		Set<Entry<String, Integer>> s=h.entrySet();
 		Iterator<Entry<String, Integer>> it=s.iterator();
 		while(it.hasNext()) {
 			if(max<it.next().getValue()) {
 				max=it.next().getValue();
 			}
-			
+			for(Entry<String, Integer> entry : h.entrySet()) {
+				if (Objects.equals(max, entry.getValue())) {
+					key= entry.getKey();
+				}
+			}
 		}
-		return max;
+		return key;
 			
 	}
 	
