a55f1e7b90aa88af80e4ce7ebe078fb7cc7bd2b3
flaviagioiello <s1087154@studenti.univpm.it>
2020-06-18 12:52:45 +0200

    aggiustato emoticon

--- FacebookApplication/FacebookApplication/src/main/java/application/model/Emoticon.java
+++ FacebookApplication/FacebookApplication/src/main/java/application/model/Emoticon.java
@@ -3,64 +3,55 @@ package application.model;
 //import java.lang.Character.UnicodeBlock;
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import application.util.Storage;
 
 /**
- * class that describes the emoticons
+ * classe che descrive le emoticons
  * 
- * @author Flavai Gioiello
+ * @author Flavia Gioiello
  * @author Alessia Castagnolo
  *
  */
 public class Emoticon extends Post {
-	static String[] emoji = {"\\u2b07\\ufe0f", "\\ud83d\\udc1d"};
 
+	/**
+	 * costruttore della classe
+	 */
 	public Emoticon() {
 		super();
-
 	}
 
 	/**
-	 * metodo che riempie l'hashmap di emoticon inserendo il nome come chiave e
-	 * quante volte appare come valore
+	 * metodo che riempie l'hashmap di emoticon inserendo il nome, ovvero il codice
+	 * unicode, come chiave e quante volte appare come valore
 	 * 
-	 * @param post, array di post
-	 * @return 
-	 * 
+	 * @return emoticon, hashmap di emoticon
 	 */
 	public static HashMap<String, Integer> count() {
 		HashMap<String, Integer> emoticon = new HashMap<>();
 		ArrayList<Post> array = Storage.get_post();
 		for (int k = 0; k < array.size(); k++) {
-			if (!array.get(k).getMessage().isEmpty()) {				
+			if (!array.get(k).getMessage().isEmpty()) {
 				String msg = array.get(k).getMessage();
-				Pattern pattern = Pattern.compile("[\ud83c\udc00-\ud83c\udfff]|[\ud83d\udc00-\ud83d\udfff]|[\u2600-\u27ff]",
-                        Pattern.UNICODE_CASE | Pattern.CASE_INSENSITIVE);				
+				Pattern pattern = Pattern.compile(
+						"[\ud83c\udc00-\ud83c\udfff]|[\ud83d\udc00-\ud83d\udfff]|[\u2600-\u27ff]|[\u2700-\u27bf]|\u2b07\ufe0f|\ud83e\udd29|\ud83e\udda0|\ud83e\udde0|\ud83d\ude34",
+						Pattern.UNICODE_CASE | Pattern.CASE_INSENSITIVE);
 				Matcher matcher = pattern.matcher(msg);
 				ArrayList<String> matchList = new ArrayList<String>();
-				while(matcher.find()) {
-					
+				while (matcher.find()) {
 					matchList.add(matcher.group());
 				}
-				for(int i = 0; i<matchList.size(); i++) {
-			
-					if(emoticon.containsKey(matchList.get(i))) {
-						int value = emoticon.get(matchList.get(i))+1;
+				for (int i = 0; i < matchList.size(); i++) {
+					if (emoticon.containsKey(matchList.get(i))) {
+						int value = emoticon.get(matchList.get(i)) + 1;
 						emoticon.put(matchList.get(i), value);
-						
-					}
-					else 
+					} else
 						emoticon.put(matchList.get(i), 1);
-				
 				}
-
 			}
-			}
-		System.out.println(emoticon);
+		}
 		return emoticon;
 	}
 }
