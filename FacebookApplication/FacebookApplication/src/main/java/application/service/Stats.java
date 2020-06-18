cbfad977f259e2c01f586eb70096e995c1169c8a
Alessia Castagnolo <a.castagnolo58@gmail.com>
2020-06-18 16:17:14 +0200

    modifica javadoc

--- FacebookApplication/FacebookApplication/src/main/java/application/service/Stats.java
+++ FacebookApplication/FacebookApplication/src/main/java/application/service/Stats.java
@@ -1,5 +1,10 @@
 package application.service;
-
+/**
+ * interfaccia per i metodi max_key() e min_key() relativi alle statistiche di hashtag ed emoticon
+ * @author Flavia Gioiello
+ * @author Alessia Castagnolo
+ *
+ */
 public interface Stats {
 	public String max_key();
 	public String min_key();
